1bf42a3858768e536b66d42898feae05b977b8d8
Fabricio Hupan <fabrihupan@gmail.com>
2024-10-24 23:19:33 -0300

    setting particules

--- src/utils/Cover-Particles.tsx
+++ src/utils/Cover-Particles.tsx
@@ -22,20 +22,7 @@ export const CoverParticles = () => {
                     options={{
                         fpsLimit: 120,
                         interactivity: {
-                            events: {
-                                onClick: {
-                                    enable: true,
-                                    mode: 'push'
-                                },
-                                onHover: {
-                                    enable: true,
-                                    mode: 'repulse'
-                                }
-                            },
                             modes: {
-                                push: {
-                                    quantity: 4
-                                },
                                 repulse: {
                                     distance: 200,
                                     duration: 0.4
@@ -76,7 +63,7 @@ export const CoverParticles = () => {
                                 type: 'circle'
                             },
                             size: {
-                                value: { min: 1, max: 5 }
+                                value: { min: 2, max: 5 }
                             }
                         },
                         detectRetina: true
